591594a9a8ffffb8c1503ca9e3e4462587cab33b
Dionatan <prof_dionatanmarkiu@camporeal.edu.br>
2026-08-24 20:45:07 -0300

    Aula 6 - Laravel Breeze

--- resources/views/produtos/index.blade.php
+++ resources/views/produtos/index.blade.php
@@ -1,28 +1,40 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Produtos</title>
-</head>
-<body>
-    <a href="{{ route('produtos.create') }}">Criar novo Produto</a>
+<x-app-layout>
 
-    @if (session('success'))
-        <p style="color: green">{{ session('success') }}</p>
-    @endif
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            Produtos
+        </h2>
+    </x-slot>
 
-    @foreach ($produtos as $produto)
-        <p>Nome: {{ $produto->nome }}</p>
-        <p>Preço: R${{ $produto->preco }}</p>
-        <p>Categoria: {{ $produto->categoria->nome }}</p> 
-        <a href="{{ route('produtos.edit', $produto) }}">Editar</a> 
-        <form action="{{ route('produtos.destroy', $produto) }}" method="POST">
-            @method('DELETE')
-            <button type="submit">Deleter</button>
-        </form>
-        <hr>    
-    @endforeach
-</body>
-</html>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    
+                    <a href="{{ route('produtos.create') }}"><x-primary-button>Criar Novo Produto</x-primary-button></a>
+
+                        @if (session('success'))
+                            <p style="color: green">{{ session('success') }}</p>
+                        @endif
+
+                        @foreach ($produtos as $produto)
+                            <p>Nome: {{ $produto->nome }}</p>
+                            <p>Preço: R${{ $produto->preco }}</p>
+                            <p>Categoria: {{ $produto->categoria->nome }}</p> 
+                            <a href="{{ route('produtos.edit', $produto) }}"><x-secondary-button>Editar</x-secondary-button></a> 
+                            <form action="{{ route('produtos.destroy', $produto) }}" method="POST">
+                                @method('DELETE')
+                                @csrf
+                                <x-danger-button type="submit">Deletar</x-danger-button>
+                            </form>
+                            <hr>    
+                        @endforeach
+                
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+</x-app-layout>
